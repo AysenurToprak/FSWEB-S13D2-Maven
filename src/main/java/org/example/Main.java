cca3d9bf8a3b1ff63756eeaef8a8f1f4b7f0ffcb
Ank <toprakaysenur96@gmail.com>
2025-01-16 15:33:11 +0300

    palindrome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,73 +1,54 @@
 package org.example;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        numberToWords(123);
-        numberToWords(1010);
-        numberToWords(-12);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Bir sayı giriniz: ");
-        int number = scanner.nextInt();
-        if (isPerfectNumber(number)){
-            System.out.println(number + " mükemmel bir sayıdır.");
-        } else {
-            System.out.println(number + " mükemmel bir sayı değildir.");
-        }
 
-        if (isPalindrome(number)){
-            System.out.println(number + " mükemmel bir sayıdır.");
-        } else {
-            System.out.println(number + " mükemmel bir sayı değildir.");
-        }
+        System.out.println("Hello world!2");
+        boolean result = isPalindrome(-1221);
+        System.out.println(result);
     }
+    public static boolean isPalindrome(int number) {
+        int newNumber = Math.abs(number);
+        String numberString = String.valueOf(newNumber);
+        String palindromeNumber = new StringBuilder(numberString).reverse().toString();
+
+        if (numberString.equals(palindromeNumber)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
 
     public static boolean isPerfectNumber(int number) {
-        int sum = 0 ;
-        for(int i = 1 ; i<=number/2;i++){
-            if (number % i == 0){
-                sum +=i;
+        int result = 0;
+
+        for (int i=1; i < number; i++) {
+            if (number % i == 0) {
+                result += i;
             }
         }
-        return  sum == number;
-    }
+        return  result == number;
+    };
 
-    public  static  boolean isPalindrome(int number){
-        int originalNumber = number;
-        int reservedNumber = 0;
-        int remainder;
-        while (number !=0){
-            remainder = number % 10;
-            reservedNumber = reservedNumber *10+remainder;
-            number = number/10;
-        }
-        return  originalNumber == reservedNumber;
-    }
-    public static short numberToWords(int number) {
+    public static String numberToWords(int number) {
+
         if (number < 0) {
-            System.out.println("Invalid Value");
-            return 0;
+            return "Invalid Value";
         }
 
         String[] words = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
 
-        if (number == 0) {
-            System.out.println("Zero");
-            return 0;
-        }
+        ArrayList<String> result = new ArrayList<>();
 
-        String result = "";
         while (number > 0) {
-            result = words[number % 10] + " " + result;
-            number /= 10;
+            int sonRakam = number % 10;
+            result.add(0,words[sonRakam]);
+            number = number / 10;
         }
 
-        System.out.println(result.trim());
-        return 0;
+        return String.join(" ", result);
     }
 }
-
-
-
-
